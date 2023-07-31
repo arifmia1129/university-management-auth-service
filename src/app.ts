cdb68a8b2bf59938ad5e0ee0f903737654c22ab3
arifmia1129 <arifmia1129@gmail.com>
2023-07-31 15:34:09 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: 'Server is running successfully',
-  });
-});
+  })
+})
 
-export default app;
+export default app
