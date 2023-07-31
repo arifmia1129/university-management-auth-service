42a613124b2f6750177554abd41ea05324be2308
arifmia1129 <arifmia1129@gmail.com>
2023-07-31 16:28:30 +0600

    test

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config';
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
-main().catch((err) => console.log(err));
+main().catch(err => console.log(err))
 
 async function main() {
-  await mongoose.connect(config.db_url as string);
-  console.log('DB Connected!');
+  await mongoose.connect(config.db_url as string)
+  console.log('DB Connected!')
 
   app.listen(config.port, () => {
-    console.log(`Application is listening on port ${config.port}`);
-  });
+    console.log(`Application is listening on port ${config.port}`)
+  })
 }
