67bd266148afa56759cc75ae05476de1052cea5c
arifmia1129 <arifmia1129@gmail.com>
2023-07-31 10:33:46 +0600

    Create .env , config ,install ts-node-dev, connect to atlas

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(config.db_url as string);
+  console.log('DB Connected!');
+
+  app.listen(config.port, () => {
+    console.log(`Application is listening on port ${config.port}`);
+  });
+}
