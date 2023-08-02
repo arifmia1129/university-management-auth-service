8d0c0f353a3495713f8df37749ec659b49bc90ce
arifmia1129 <arifmia1129@gmail.com>
2023-08-02 19:43:55 +0600

    [DONE] created user services

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,23 +1,28 @@
 import { Schema, model } from 'mongoose'
 import { IUser, IUserMethods, UserModel } from './user.interface'
 
-const userSchema = new Schema<IUser, UserModel, IUserMethods>({
-  id: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema<IUser, UserModel, IUserMethods>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['student', 'admin', 'faculty'],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
   },
-  role: {
-    type: String,
-    required: true,
-    enum: ['student', 'admin', 'faculty'],
+  {
+    timestamps: true,
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-})
+)
 
 const User = model<IUser, UserModel>('User', userSchema)
 
